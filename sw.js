6cddeb9aea79d120150cb0fdfef0eea9e43174d0
3323jaydenjayden-sketch <3323jaydenjayden@gmail.com>
2026-09-15 22:26:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'spotify-lite-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/api.js',
+  './js/app.js',
+  './manifest.json'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('pipedapi')) return;
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
